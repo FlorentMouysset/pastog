5d3bdedb52a7f1b3f9ed98621aab212df8029760
medi24 <mehdi-but@hotmail.fr>
2014-01-30 16:44:03 +0100

    fonctionnel without test #16

--- grails-app/domain/ups/pastog/SubDomain.groovy
+++ grails-app/domain/ups/pastog/SubDomain.groovy
@@ -2,10 +2,12 @@ package ups.pastog
 class SubDomain {
 
 	User user
+    String label
 	static belongsTo = Domain
 
 	static hasMany=[ virtualEvent: VirtualEvent,  physicalEvent: PhysicalEvent ]
 	
 	    static constraints = {
+
     }
 }
