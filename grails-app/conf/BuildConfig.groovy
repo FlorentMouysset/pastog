e1c0acf06e41169fe69d91ffb0b96f38fbcbdec7
Florent Mouysset <mouysset.florent@gmail.com>
2014-02-03 23:09:19 +0100

    clean BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -78,7 +78,7 @@ grails.project.dependency.resolution = {
         runtime ":resources:1.2.1"
     	compile ':cache:1.1.1'
 		compile ":codenarc:0.19"
-	//	compile ":spring-security-core:1.2.7.3"
+		//compile ":spring-security-core:1.2.7.3"
 		
 		test ":geb:$gebVersion"
 		compile ":geb:$gebVersion"
